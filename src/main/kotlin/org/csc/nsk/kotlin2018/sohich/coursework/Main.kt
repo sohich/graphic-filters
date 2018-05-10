3d3b7077eecb75a2d8a9cb6ba8fa637e1e10025f
Roman Posokhin <posohin@ccfit.nsu.ru>
2018-05-10 15:28:33 +0700

    Usage: added tab whitespaces to align options' description

--- src/main/kotlin/org/csc/nsk/kotlin2018/sohich/coursework/Main.kt
+++ src/main/kotlin/org/csc/nsk/kotlin2018/sohich/coursework/Main.kt
@@ -12,17 +12,17 @@ private object Helper {
     const val usageString = "Usage:\n" +
                             "filters <source image path> [-option [arg]] <target image path>\n\n" +
                             "Options supported:\n" +
-                            "-negate: replace each pixel with its complementary color;\n" +
-                            "-grayscale: convert image to grayscale;\n" +
-                            "-rotate degrees{<}{>}: apply image rotation to the image,\n" +
+                            "-negate\t\t\t\t\t: replace each pixel with its complementary color;\n" +
+                            "-grayscale\t\t\t\t: convert image to grayscale;\n" +
+                            "-rotate degrees{<}{>}\t: apply image rotation to the image,\n" +
                             "< rotates the image only if its width is less than the height, > does contrariwise;\n" +
-                            "-gamma value: apply gamma correction to the image;\n" +
-                            "-median range: apply median filter with given range to the image;\n" +
-                            "-emboss: emboss the image;\n" +
-                            "-dither red green blue: apply a Floyd-Steinberg error diffusion dither to image,\n" +
+                            "-gamma value\t\t\t: apply gamma correction to the image;\n" +
+                            "-median range\t\t\t: apply median filter with given range to the image;\n" +
+                            "-emboss\t\t\t\t\t: emboss the image;\n" +
+                            "-dither red green blue\t: apply a Floyd-Steinberg error diffusion dither to image,\n" +
                             "input parameters set count of shades in each channel's palette;\n" +
-                            "-ordered-dither: dither the image using a pre-defined ordered dither;\n" +
-                            "-sharpen: sharpen the image;\n" +
-                            "-watercolor: watercolor the image;" +
-                            "-zoom: double the image at both of width and height\n"
+                            "-ordered-dither\t\t\t: dither the image using a pre-defined ordered dither;\n" +
+                            "-sharpen\t\t\t\t: sharpen the image;\n" +
+                            "-watercolor\t\t\t\t: watercolor the image;\n" +
+                            "-zoom\t\t\t\t\t: double the image at both of width and height\n"
 }
